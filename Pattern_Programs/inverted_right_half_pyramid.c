718f4679dea20fa4a718d01b258a36b7494694e1
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-12 20:16:18 +0530

    Added comment In Program

--- Pattern_Programs/inverted_right_half_pyramid.c
+++ Pattern_Programs/inverted_right_half_pyramid.c
@@ -5,6 +5,7 @@ int main(){
     printf("Enter Num Of Elements:");
     scanf("%d",&n);
 
+    //For Printing ***** pattern
     for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
             printf("* ");
