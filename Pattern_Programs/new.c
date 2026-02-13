50e344326c7ff51f6ed48b1cfa984ad86c3033b5
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-13 21:42:58 +0530

    Add Condition For checking Number

--- Pattern_Programs/new.c
+++ Pattern_Programs/new.c
@@ -5,6 +5,10 @@ int main(){
     // Pattern For Increasing Number Triangle
     printf("Enter the Num Of Elements:");
     scanf("%d",&m);
+    if(m<=0){
+        printf("Enetr  Number Greater Than 0...");
+        return 0;
+    }
 
     for(int i=1;i<=m;i++){
         for(int j=1;j<=i;j++){
