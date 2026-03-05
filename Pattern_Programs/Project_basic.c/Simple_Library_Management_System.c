a6291d586af8cbd765a404760446cb5b2d929d27
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-05 20:46:48 +0530

    Add Display And Switch In Project

--- Pattern_Programs/Project_basic.c/Simple_Library_Management_System.c
+++ Pattern_Programs/Project_basic.c/Simple_Library_Management_System.c
@@ -10,6 +10,26 @@ struct Book library[50];
 int count = 0;
 
 int main(){
+      int choice;
+
+    while (1) {
+        printf("\n===== Library System =====\n");
+        printf("1. Add Book\n");
+        printf("2. Show Books\n");
+        printf("3. Search Book\n");
+        printf("4. Exit\n");
+
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: addBook(); break;
+            case 2: showBooks(); break;
+            case 3: searchBook(); break;
+            case 4: return 0;
+            default: printf("Invalid choice!\n");
+        }
+    }
 
     return 0;
 }
