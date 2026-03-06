2929abd7fa75a49c803d4a9ee557e60da49cbd96
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-06 17:47:43 +0530

    Handle Edge Case In addBook Function

--- Pattern_Programs/Project_basic.c/Simple_Library_Management_System.c
+++ Pattern_Programs/Project_basic.c/Simple_Library_Management_System.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-struct Book {
+struct Book
+{
     int id;
     char name[50];
     char author[50];
@@ -9,7 +10,14 @@ struct Book {
 struct Book library[50];
 int count = 0;
 
-void addBook() {
+void addBook()
+{
+
+    if (count >= 50)
+    {
+        printf("Library is full!\n");
+        return;
+    }
     printf("\nEnter Book ID: ");
     scanf("%d", &library[count].id);
 
@@ -22,16 +30,19 @@ void addBook() {
     count++;
     printf("Book added successfully!\n");
 }
-void showBooks() {
+void showBooks()
+{
     int i;
 
-    if (count == 0) {
+    if (count == 0)
+    {
         printf("\nNo books available.\n");
         return;
     }
 
     printf("\n--- Book List ---\n");
-    for (i = 0; i < count; i++) {
+    for (i = 0; i < count; i++)
+    {
         printf("ID: %d | Name: %s | Author: %s\n",
                library[i].id,
                library[i].name,
@@ -39,14 +50,17 @@ void showBooks() {
     }
 }
 
-void searchBook() {
+void searchBook()
+{
     int id, i, found = 0;
 
     printf("\nEnter Book ID to search: ");
     scanf("%d", &id);
 
-    for (i = 0; i < count; i++) {
-        if (library[i].id == id) {
+    for (i = 0; i < count; i++)
+    {
+        if (library[i].id == id)
+        {
             printf("Book Found!\n");
             printf("Name: %s | Author: %s\n",
                    library[i].name,
@@ -60,10 +74,12 @@ void searchBook() {
         printf("Book not found!\n");
 }
 
-int main(){
-      int choice;
+int main()
+{
+    int choice;
 
-    while (1) {
+    while (1)
+    {
         printf("\n===== Library System =====\n");
         printf("1. Add Book\n");
         printf("2. Show Books\n");
@@ -73,12 +89,21 @@ int main(){
         printf("Enter choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1: addBook(); break;
-            case 2: showBooks(); break;
-            case 3: searchBook(); break;
-            case 4: return 0;
-            default: printf("Invalid choice!\n");
+        switch (choice)
+        {
+        case 1:
+            addBook();
+            break;
+        case 2:
+            showBooks();
+            break;
+        case 3:
+            searchBook();
+            break;
+        case 4:
+            return 0;
+        default:
+            printf("Invalid choice!\n");
         }
     }
 
